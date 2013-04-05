8aab1c8b4d02f1e9d561a559a3195d1136949ca9
Stian Veum Mollersen <stian.moellersen@gmail.com>
2013-04-05 14:01:14 +0200

    Added install-script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ln -s .bashrc ~/.bashrc
+ln -s .bash_prompt ~/.bash_prompt
+ln -s .bash_profile ~/.bash_profile
+ln -s .aliases ~/.aliases
+ln -s .extra ~/.extra
+ln -s .exports ~/.exports
+ln -s .functions ~/.functions
+ln -s .gitconfig ~/.gitconfig
