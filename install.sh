15fae9d433ed16927ea355f12bfc8a89b7283c02
Stian Veum Møllersen <stian.moellersen@gmail.com>
2017-02-12 16:52:36 +0100

    Remove unused bash-stuff

--- install.sh
+++ install.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-for file in aliases gitconfig inputrc exports extra functions; do
+for file in zshrc zsh_prompt aliases gitconfig inputrc exports extra functions; do
 	rm -f ~/.$file
 	ln -s $PWD/$file ~/.$file
 done
-
-if [ "$1" = "" ]; then
-	for file in zshrc zsh_prompt; do
-		rm -f ~/.$file
-		ln -s $PWD/$file ~/.$file
-	done
-elif [ "$1" = "bash" ]; then
-	for file in bashrc bash_profile bash_prompt; do
-		rm -f ~/.$file
-		ln -s $PWD/$file ~/.$file
-	done	
-fi
 
 touch ~/.bash_history
