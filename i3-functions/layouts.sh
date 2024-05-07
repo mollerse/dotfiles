ce2b4d65f370732ab8f24948d50dc4bcab22abc7
Stian Veum Møllersen <stian.moellersen@gmail.com>
2024-05-07 14:29:19 +0200

    new laptop

--- i3-functions/layouts.sh
+++ i3-functions/layouts.sh
@@ -2,31 +2,31 @@
 
 case "$1" in
   "default")
-    xrandr --output eDP1 --mode 2560x1440
-    xrandr --output DP1-1 --off
-    xrandr --output DP1-2 --off
+    xrandr --output eDP --mode 2560x1440
+    xrandr --output DisplayPort-6 --off
+    xrandr --output DisplayPort-7 --off
     xrandr --output HDMI2 --off
     ;;
   "laptop")
-    xrandr --output eDP1 --mode 1920x1080
-    xrandr --output DP1-1 --off
-    xrandr --output DP1-2 --off
+    xrandr --output eDP --mode 1920x1080
+    xrandr --output DisplayPort-6 --off
+    xrandr --output DisplayPort-7 --off
     xrandr --output HDMI2 --off
     ;;
   "twoscreens")
-    xrandr --output eDP1 --off
-    xrandr --output DP1-2 --auto --primary
-    xrandr --output DP1-1 --auto --left-of DP1-2 --rotate right
-    xrandr --output DP1-2 --pos 1440x560
+    xrandr --output eDP --off
+    xrandr --output DisplayPort-7 --auto --primary
+    xrandr --output DisplayPort-6 --auto --left-of DisplayPort-7 --rotate right
+    xrandr --output DisplayPort-7 --pos 1440x560
 
-    i3-msg "workspace number 1, move workspace to output DP1-2"
-    i3-msg "workspace number 2, move workspace to output DP1-2"
-    i3-msg "workspace number 3, move workspace to output DP1-2"
-    i3-msg "workspace number 4, move workspace to output DP1-1"
+    i3-msg "workspace number 1, move workspace to output DisplayPort-7"
+    i3-msg "workspace number 2, move workspace to output DisplayPort-7"
+    i3-msg "workspace number 3, move workspace to output DisplayPort-7"
+    i3-msg "workspace number 4, move workspace to output DisplayPort-6"
     ;;
   "onescreen")
     xrandr --output HDMI2 --auto --primary
-    xrandr --output eDP1 --off
+    xrandr --output eDP --off
 
     i3-msg "workspace number 1, move workspace to output HDMI2"
     i3-msg "workspace number 2, move workspace to output HDMI2"
@@ -34,8 +34,8 @@ case "$1" in
     i3-msg "workspace number 4, move workspace to output HDMI2"
     ;;
   "presentation")
-    xrandr --output eDP1 --primary --mode 2560x1440
-    xrandr --output HDMI2 --auto --above eDP1
+    xrandr --output eDP --primary --mode 2560x1440
+    xrandr --output HDMI2 --auto --above eDP
     ;;
   *)
     exit 0
